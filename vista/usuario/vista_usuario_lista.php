6a67e2daf2daa1fd076e9a16af421910a1ab2ed2
Carlos Aguilar <carlos.aguilar_amk@outlook.com>
2024-06-09 19:47:02 -0600

    la carpeta iba dentro del xampp

---
+++ vista/usuario/vista_usuario_lista.php
@@ -0,0 +1,18 @@
+<div class="box box-warning box-solid">
+            <div class="box-header with-border">
+              <h3 class="box-title">BIENVENIDO AL CONTENIDO usuario</h3>
+
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+              </div>
+              <!-- /.box-tools -->
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              CONTENIDO PRINCIPAL usuario
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
